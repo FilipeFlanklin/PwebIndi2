14da1bfdb62aa2bb9ae2991f3c2ff960bccc1986
Filipe Flanklin Ferreira de Sousa <87499222+FilipeFlanklin@users.noreply.github.com>
2024-06-16 11:37:36 -0200

    Update livro.js

--- livro.js
+++ livro.js
@@ -3,32 +3,31 @@ const fs = require('fs');
 const router = express.Router();
 const filePath = './livros.json';
 
-// auxiliar para ler o arquivo JSON
-function readBooksFromFile() {
+//arquivo JSON
+function LerArquivo() {
     const data = fs.readFileSync(filePath);
     return JSON.parse(data);
 }
-
-// auxiliar para escrever no arquivo JSON
-function writeBooksToFile(books) {
+// escrever no arquivo JSON
+function EditarArquivo(books) {
     fs.writeFileSync(filePath, JSON.stringify(books, null, 2));
 }
 
 // Listagem
 router.get('/', (req, res) => {
-    const books = readBooksFromFile();
+    const books = LerArquivo();
     res.json(books);
 });
 
 // Compra
 router.post('./comprar', (req, res) => {
     const { nome } = req.body;
-    const books = readBooksFromFile();
+    const books = LerArquivo();
 
     const book = books.find(b => b.nome === nome);
     if (book && book.exemplares > 0) {
         book.exemplares -= 1;
-        writeBooksToFile(books);
+        EditarArquivo(books);
         res.status(200).send('Livro comprado com sucesso!');
     } else {
         res.status(400).send('Livro não disponível.');
@@ -38,10 +37,10 @@ router.post('./comprar', (req, res) => {
 // Cadastro
 router.post('/', (req, res) => {
     const { nome, autor, genero, exemplares, imagem } = req.body;
-    const books = readBooksFromFile();
+    const books = LerArquivo();
 
     books.push({ nome, autor, genero, exemplares, imagem });
-    writeBooksToFile(books);
+    EditarArquivo(books);
     res.status(201).send('Livro cadastrado com sucesso!');
 });
 
